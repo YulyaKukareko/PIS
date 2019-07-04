e2a1e605472c0b1bad863f4da06bbcb8cf0b1ef0
YulyaKukareko <yulya.kukareko@mail.ru>
2019-07-04 13:58:24 +0300

    PIS

---
+++ Lab_6/Lab6_2/WcfServiceLibrary/Client/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        ServiceWCF.Service1Client client;
+        public Form1()
+        {
+            InitializeComponent();
+            this.client = new ServiceWCF.Service1Client();
+            this.client.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result.Text = this.client.Sum(Int32.Parse(x.Text), Int32.Parse(y.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result.Text = this.client.Sub(Int32.Parse(x.Text), Int32.Parse(y.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            result.Text = this.client.ConCat(x.Text, y.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.client.Close();
+        }
+
+        private void y_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab_6/Lab6_2/WcfServiceLibrary/WcfServiceLibrary/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : IService1
+    {
+        public string ConCat(string x, string y)
+        {
+            return x + y;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
